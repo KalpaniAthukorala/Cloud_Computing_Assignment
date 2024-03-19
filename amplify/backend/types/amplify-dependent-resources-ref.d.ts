de6bc324e9c98044d230fea6f40b6d91c64e38b7
KalpaniSachie <kalpani.athukorala@oloidtechnologies.com>
2024-03-19 20:27:22 +0530

    User-management-auth-creation

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cloudassignment0a8491ed": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
